bbcc4d266d7f33ce47a2296c20582cebfbc87332
Robert Radkiewicz <robert.radkiewicz@magicline.de>
2015-08-31 16:35:27 +0200

    add psql 9.4 to path

--- system/path.zsh
+++ system/path.zsh
@@ -3,5 +3,8 @@ export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/
 if [ -d "/Applications/Postgres.app/Contents/Versions/9.3/bin" ]; then
 	export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
 fi
+if [ -d "/Applications/Postgres.app/Contents/Versions/9.4/bin" ]; then
+	export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
+fi
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
