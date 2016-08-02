3fe93f063e1286dbf8804380d908c416cc0c03a7
Robert Radkiewicz <robert.radkiewicz@magicline.de>
2016-08-02 16:09:24 +0200

    add psql 9.5

--- system/path.zsh
+++ system/path.zsh
@@ -6,5 +6,8 @@ fi
 if [ -d "/Applications/Postgres.app/Contents/Versions/9.4/bin" ]; then
 	export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
 fi
+if [ -d "/Applications/Postgres.app/Contents/Versions/9.5/bin" ]; then
+	export PATH="/Applications/Postgres.app/Contents/Versions/9.5/bin:$PATH"
+fi
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
