0b461b3e0c7f718587abdeee142db8e15cb470e4
Robert Radkiewicz <robert.radkiewicz@magicline.de>
2018-03-12 11:25:47 +0100

    Remove old npm reference

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/bin/npm:$ZSH/bin:$HOME/.bin:$PATH"
+export PATH="$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$PATH"
 
 if [ -d "/Applications/Postgres.app/Contents/Versions/9.6/bin" ]; then
 	export PATH="/Applications/Postgres.app/Contents/Versions/9.6/bin:$PATH"
