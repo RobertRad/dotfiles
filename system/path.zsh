a6fd7d18bdefb07e6d08742ddbced54ed79d477b
Robert Radkiewicz <robert.radkiewicz@magicline.de>
2018-03-12 11:23:58 +0100

    Remove old Postgres entries

--- system/path.zsh
+++ system/path.zsh
@@ -1,14 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/bin/npm:$ZSH/bin:$HOME/.bin:$PATH"
 
-if [ -d "/Applications/Postgres.app/Contents/Versions/9.3/bin" ]; then
-	export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
-fi
-if [ -d "/Applications/Postgres.app/Contents/Versions/9.4/bin" ]; then
-	export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
-fi
-if [ -d "/Applications/Postgres.app/Contents/Versions/9.5/bin" ]; then
-	export PATH="/Applications/Postgres.app/Contents/Versions/9.5/bin:$PATH"
-fi
 if [ -d "/Applications/Postgres.app/Contents/Versions/9.6/bin" ]; then
 	export PATH="/Applications/Postgres.app/Contents/Versions/9.6/bin:$PATH"
 fi
