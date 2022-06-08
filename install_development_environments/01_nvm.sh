3972c022a164719537ef457d5f54fbd13c085d87
robert <robert>
2022-06-08 12:31:41 +0200

    zsh-nvm plugin

--- install_development_environments/01_nvm.sh
+++ install_development_environments/01_nvm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NVM_VERSION="v0.39.1"
 
-echo "Download nvm"
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh" | bash
+echo "Download zsh-nvm plugin"
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 
 echo "Done"
 echo "Now open a new console and type 'nvm install <my_version>' && nvm use <my_version>'"
