f4ece45fc650a44d682be25c75ad19a21eee6ea5
Robert Kindermann <r.kindermann@hhpv.de>
2022-06-21 10:58:42 +0200

    OpenJDK Semeru

--- install_development_environments/02_openjdk_temurin.sh
+++ install_development_environments/02_openjdk_temurin.sh
@@ -5,13 +5,13 @@ export MAIN_VERSION=${VERSION:0:2}
 export URL_VERSION=${VERSION/_/%2B}
 export FILE_VERSION=${VERSION/_/+}
 
-echo "Download OpenJDK Temurin"
+echo "Download OpenJDK Temurin ${VERSION}"
 curl -L "https://github.com/adoptium/temurin${MAIN_VERSION}-binaries/releases/download/jdk-${URL_VERSION}/OpenJDK${MAIN_VERSION}U-jdk_x64_linux_hotspot_${VERSION}.tar.gz" -o OpenJDK.tar.gz
 tar -xf OpenJDK.tar.gz
-mv jdk-18.0.1+10 ~
-ln -s ~/jdk-${FILE_VERSION}/bin/java ~/.local/bin
-ln -s ~/jdk-${FILE_VERSION}/bin/javac ~/.local/bin
-ln -s ~/jdk-${FILE_VERSION}/bin/jconsole ~/.local/bin
+mv jdk-${FILE_VERSION} ~/jdk-temurin-${FILE_VERSION}
+ln -s ~/jdk-temurin-${FILE_VERSION}/bin/java ~/.local/bin
+ln -s ~/jdk-temurin-${FILE_VERSION}/bin/javac ~/.local/bin
+ln -s ~/jdk-temurin-${FILE_VERSION}/bin/jconsole ~/.local/bin
 rm OpenJDK.tar.gz
 
 echo "Done"
---
+++ install_development_environments/03_openjdk_semeru.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export VERSION="18.0.1.1_2"
+export OPENJ9_VERSION="0.32.0"
+
+export MAIN_VERSION=${VERSION:0:2}
+export URL_VERSION=${VERSION/_/%2B}
+export FILE_VERSION=${VERSION/_/+}
+
+echo "Download OpenJDK Semeru ${VERSION}"
+curl -L "https://github.com/ibmruntimes/semeru${MAIN_VERSION}-binaries/releases/download/jdk-${URL_VERSION}_openj9-${OPENJ9_VERSION}/ibm-semeru-open-jdk_x64_linux_${VERSION}_openj9-${OPENJ9_VERSION}.tar.gz" -o OpenJDK.tar.gz
+tar -xf OpenJDK.tar.gz
+mv jdk-${FILE_VERSION} ~/jdk-semeru-${FILE_VERSION}
+ln -s ~/jdk-semeru-${FILE_VERSION}/bin/java ~/.local/bin
+ln -s ~/jdk-semeru-${FILE_VERSION}/bin/javac ~/.local/bin
+ln -s ~/jdk-semeru-${FILE_VERSION}/bin/jconsole ~/.local/bin
+rm OpenJDK.tar.gz
+
+echo "Done"
+
