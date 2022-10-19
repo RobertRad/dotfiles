068b83d079f604750b0c6afef6285a911f458542
Robert Kindermann <r.kindermann@hhpv.de>
2022-10-19 21:53:19 +0200

    New k9s version

--- install_tools_on_debian/05_k9s.sh
+++ install_tools_on_debian/05_k9s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION="v0.26.6"
+export VERSION="v0.26.7"
 
 echo "Downloading k9s"
 curl -L "https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz" -o k9s.tar.gz
